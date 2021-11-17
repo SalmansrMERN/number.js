bcb2ef83baf7881386d4fb02d62929130ffa844e
Salman S. Rahman <salmansr1995@gmail.com>
2021-11-17 23:08:03 +0600

    first commit

---
+++ number.js
@@ -0,0 +1,21 @@
+// ====== transfer into integr number ====
+var num = "20.80";
+console.log(typeof (num));
+num = parseInt(num);
+console.log(typeof (num));
+console.log(num)
+
+// ====== transfer into float number =====
+
+var num1 = "30.90";
+var num2 = parseFloat(num1);
+console.log(num2);
+
+// ====== transfer into number =====
+
+var num3 = "50"
+num3 = Number(num3);
+console.log(num3)
+
+
+
